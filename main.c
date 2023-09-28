0cfd9dec5881fd19b68d98bfe5ed3d43c9780096
Joseph <josephdrl2005@gmail.com>
2023-09-28 16:06:54 -0300

    3rd_Version

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int IN3 = 8;
 int IN4 = 9;
 int ENB = 10;
 int VELOCIDAD;			// variable para almacenar valor de velocidad
-int VMotor = 255;
+int VMotor1 = 255;
 int VMotor2 = 255;
 
 int main() {
@@ -34,16 +34,31 @@ pinMode(SCL,INPUT);
   pinMode(IN3, OUTPUT);		// pin 8 como salida
   pinMode(IN4, OUTPUT);		// pin 9 como salida	
   pinMode(ENB, OUTPUT);		// pin 10 como salida
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
 }
 while(1){
 do{
     V1 = analogRead(FDCI);
 switch(V1){
     case 1:
+     for (VELOCIDAD = 0; VELOCIDAD < VMotor1; VELOCIDAD++){	// bucle que incrementa de a uno
+  analogWrite(ENA, VELOCIDAD);				// el valor de velocidad y aplica a ENA
+  digitalWrite(IN1, LOW);				// IN1 en 0
+  digitalWrite(IN2, HIGH);	
     do{
+  for (VELOCIDAD = 0; VELOCIDAD < VMotor1; VELOCIDAD++){	// bucle que incrementa de a uno
+  analogWrite(ENA, VELOCIDAD);				// el valor de velocidad y aplica a ENA
+  digitalWrite(IN1, LOW);				// IN1 en 0
+  digitalWrite(IN2, HIGH);	
         analogRead(opticS1)
     }while(opticS1 == 1);
     //apagado de cinta secundaria
+    for (VELOCIDAD = 0; VELOCIDAD < 10; VELOCIDAD++){	// bucle que incrementa de a uno
+    analogWrite(ENB, VELOCIDAD);				// el valor de velocidad y aplica a ENB
+    digitalWrite(IN3, LOW);				// IN3 en 0
+    digitalWrite(IN4, HIGH);	
 
     do{
         do{
@@ -51,11 +66,20 @@ switch(V1){
         switch(decMetal){
             case 1:
             //Se detecto metal proceso detenido;
+  lcd.setCursor(0,0);
+  lcd.print("METAL DETECTADO!!"); 
+  lcd.setCursor (0,1);
+  lcd.print("PROCESO DETENIDO!");
  for (VELOCIDAD = 0; VELOCIDAD < 10; VELOCIDAD++){	// bucle que incrementa de a uno
   analogWrite(ENA, VELOCIDAD);				// el valor de velocidad y aplica a ENA
   digitalWrite(IN1, LOW);				// IN1 en 0
   digitalWrite(IN2, HIGH);	
   }
+  for (VELOCIDAD = 0; VELOCIDAD < 10; VELOCIDAD++){	// bucle que incrementa de a uno
+  analogWrite(ENB, VELOCIDAD);				// el valor de velocidad y aplica a ENB
+  digitalWrite(IN3, LOW);				// IN3 en 0
+  digitalWrite(IN4, HIGH);	
+  } 
             break;
             default: 
             counter = 0;
@@ -63,9 +87,18 @@ switch(V1){
         }
         LCD = opticS2;
         //mostrar por pantalla el cónteo
+  lcd.setCursor(0,0);
+  lcd.print("==>ENCENDIDO!<=="); 
+  lcd.setCursor (0,1);
+  lcd.print("PR:%d",counter);
         }while(opticS2 >= 5);
     }while(opticS2 == 5);
     //continua caja
+     for (VELOCIDAD = 0; VELOCIDAD < VMotor2; VELOCIDAD++){	// bucle que incrementa de a uno
+  analogWrite(ENB, VELOCIDAD);				// el valor de velocidad y aplica a ENA
+  digitalWrite(IN3, LOW);				// IN3 en 0
+  digitalWrite(IN4, HIGH);	
+  }
     do{
         analogRead(FDCF);
     }while(FDCF == 1);
